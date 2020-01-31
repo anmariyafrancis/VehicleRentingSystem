4c9f854a13294cb13eed0785c61a2671ce8df549
anmariya <anmariya1915@gmail.com>
2020-01-30 21:13:10 -0500

    [ANMARIYA]abstract class vehicle added

--- src/main/java/com/lambton/Vehicles.java
+++ src/main/java/com/lambton/Vehicles.java
@@ -1,18 +1,18 @@
 package com.lambton;
 
-enum FuelType
+enum FuelTypes
 {
     Petrol,
     Diesel,
     Electric
 }
 
-public abstract class Vehicle
+public abstract class Vehicles
 {
     private int vechicleId;
     private String vehicleDetails;
     private String manufacturerName;
     private String insuranceProviderName;
-    private int noOfSeat;
-    private FuelType fuelType;
+    private int noOfSeats;
+    private FuelTypes fuelTypes;
 }
