a3feaffe455c157716adf6748a16248e005a30f2
anmariya <anmariya1915@gmail.com>
2020-01-30 21:14:56 -0500

    [ANMARIYA]getter and setter added to vehicle class

--- src/main/java/com/lambton/Vehicles.java
+++ src/main/java/com/lambton/Vehicles.java
@@ -15,4 +15,52 @@ public abstract class Vehicles
     private String insuranceProviderName;
     private int noOfSeats;
     private FuelTypes fuelTypes;
+
+    public int getVechicleId() {
+        return vechicleId;
+    }
+
+    public void setVechicleId(int vechicleId) {
+        this.vechicleId = vechicleId;
+    }
+
+    public String getVehicleDetails() {
+        return vehicleDetails;
+    }
+
+    public void setVehicleDetails(String vehicleDetails) {
+        this.vehicleDetails = vehicleDetails;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public void setManufacturerName(String manufacturerName) {
+        this.manufacturerName = manufacturerName;
+    }
+
+    public String getInsuranceProviderName() {
+        return insuranceProviderName;
+    }
+
+    public void setInsuranceProviderName(String insuranceProviderName) {
+        this.insuranceProviderName = insuranceProviderName;
+    }
+
+    public int getNoOfSeats() {
+        return noOfSeats;
+    }
+
+    public void setNoOfSeats(int noOfSeats) {
+        this.noOfSeats = noOfSeats;
+    }
+
+    public FuelTypes getFuelTypes() {
+        return fuelTypes;
+    }
+
+    public void setFuelTypes(FuelTypes fuelTypes) {
+        this.fuelTypes = fuelTypes;
+    }
 }
