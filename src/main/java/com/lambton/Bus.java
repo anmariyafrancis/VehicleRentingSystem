43c02a853ca0ce73a6d2183504be7e5ef12cd84d
anmariya <anmariya1915@gmail.com>
2020-01-31 21:20:02 -0500

    [ANMARIYA]Bus subclass created

---
+++ src/main/java/com/lambton/Bus.java
@@ -0,0 +1,9 @@
+package com.lambton;
+
+public class Bus extends Vehicles
+{
+    public Bus(int vechicleId, String vehicleDetails, String manufacturerName, String insuranceProviderName, int noOfSeats, FuelTypes fuelTypes)
+    {
+        super(vechicleId, vehicleDetails, manufacturerName, insuranceProviderName, noOfSeats, fuelTypes);
+    }
+}
