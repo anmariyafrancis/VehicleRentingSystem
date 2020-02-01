d42d8fa343742705cd6ea7a1db34f16f575b97df
philip <jhasondeleon@gmail.com>
2020-01-31 20:55:21 -0500

    [Philip] Class Customer created

---
+++ src/main/java/com/lambton/Customer.java
@@ -0,0 +1,10 @@
+package com.lambton;
+
+public class Customer extends Person
+{
+    private String address;
+    private String city;
+    private String vehicleRent;
+
+
+}
--- src/main/java/com/lambton/Person.java
+++ src/main/java/com/lambton/Person.java
@@ -125,3 +125,5 @@ public abstract class Person
         return age;
     }
 }
+
+
