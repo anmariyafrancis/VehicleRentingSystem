671c49837a170293b3683f4fe3f063b78e022e2c
philip <jhasondeleon@gmail.com>
2020-01-31 20:56:07 -0500

    [Philip] Class Customer Constructor added

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -1,10 +1,17 @@
 package com.lambton;
 
+import java.time.LocalDate;
+
 public class Customer extends Person
 {
     private String address;
     private String city;
     private String vehicleRent;
 
-
+    public Customer(int id, String firstName, String lastName, Gender gender, LocalDate birthDate, int mobileNumber, String emailId, String userName, String passWord, String address, String city, String vehicleRent) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, passWord);
+        this.address = address;
+        this.city = city;
+        this.vehicleRent = vehicleRent;
+    }
 }
