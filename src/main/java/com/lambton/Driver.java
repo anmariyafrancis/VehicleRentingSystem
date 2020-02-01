a38f1fead060d742020a6d79043635bbcf952b06
philip <jhasondeleon@gmail.com>
2020-01-31 21:02:13 -0500

    [Philip] Class Driver constructor added

--- src/main/java/com/lambton/Driver.java
+++ src/main/java/com/lambton/Driver.java
@@ -1,10 +1,18 @@
 package com.lambton;
 
+import java.time.LocalDate;
+
 public class Driver extends Person
 {
     private String driverLicenceNo;
     private boolean drivingHistoryCleared;
     private float salary;
 
+    public Driver(int id, String firstName, String lastName, Gender gender, LocalDate birthDate, int mobileNumber, String emailId, String userName, String passWord, String driverLicenceNo, boolean drivingHistoryCleared, float salary) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, passWord);
+        this.driverLicenceNo = driverLicenceNo;
+        this.drivingHistoryCleared = drivingHistoryCleared;
+        this.salary = salary;
+    }
 
 }
