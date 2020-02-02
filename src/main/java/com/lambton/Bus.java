30ddb3234ef94eb2b2fc5b43a05e417254ebd1f3
anmariya <anmariya1915@gmail.com>
2020-02-01 20:38:17 -0500

    [ANMARIYA]Getter and setter added to bus class

--- src/main/java/com/lambton/Bus.java
+++ src/main/java/com/lambton/Bus.java
@@ -16,9 +16,35 @@ enum WifiAccessibility
 public class Bus extends Vehicles implements IDisplay
 {
     private ArrayList<String> busType=new ArrayList<String>();
+    private BusAccessibility busAccess;
+    private WifiAccessibility wifiAccess;
 
     public Bus(int vechicleId, String vehicleDetails, String manufacturerName, String insuranceProviderName, int noOfSeats, FuelTypes fuelTypes)
     {
         super(vechicleId, vehicleDetails, manufacturerName, insuranceProviderName, noOfSeats, fuelTypes);
     }
+    public ArrayList<String> getBusType() {
+        return busType;
+    }
+
+    public void setBusType(ArrayList<String> busType) {
+        this.busType = busType;
+    }
+
+    public BusAccessibility getBusAccess() {
+        return busAccess;
+    }
+
+    public void setBusAccess(BusAccessibility busAccess) {
+        this.busAccess = busAccess;
+    }
+
+    public WifiAccessibility getWifiAccess() {
+        return wifiAccess;
+    }
+
+    public void setWifiAccess(WifiAccessibility wifiAccess) {
+        this.wifiAccess = wifiAccess;
+    }
+
 }
