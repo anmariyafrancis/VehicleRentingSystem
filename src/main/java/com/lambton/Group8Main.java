95f07ebae53100404c849c873eb103e2be6f6126
anmariya <anmariya1915@gmail.com>
2020-02-03 01:51:14 -0500

    [ANMARIYA]object created for main class

--- src/main/java/com/lambton/Group8Main.java
+++ src/main/java/com/lambton/Group8Main.java
@@ -4,21 +4,11 @@ public class Group8Main
 {
     public static void main(String[] args)
     {
-        Owner[] listOfOwners=new Owner[10];
-        listOfOwners[0]=owner1;
-        listOfOwners[1]=owner1;
-        listOfOwners[2]=owner1;
-        listOfOwners[3]=owner1;
-        listOfOwners[4]=owner1;
-        listOfOwners[5]=owner1;
-        listOfOwners[6]=owner1;
-        listOfOwners[0]=owner1;
-        listOfOwners[0]=owner1;
-        listOfOwners[0]=owner1;
-
-        Customer[] listOfCustomers=new Customer[10];
-        Driver[] listOfDrivers=new Driver[10];
-
+        Group8Main v1=new Group8Main();
+        v1.insertOwnerDetails();
+    }
+    public void insertOwnerDetails()
+    {
 
     }
 }
