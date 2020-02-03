7d1ffae5c62b6e8214be1bc50049fa3c767c78fc
anmariya <anmariya1915@gmail.com>
2020-02-02 23:49:12 -0500

    [ANMARIYA]local variables and getter and setter added to VehicleRent

--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -1,4 +1,67 @@
 package com.lambton;
 
-public class VehicleRent {
+import java.time.LocalDate;
+
+public class VehicleRent implements IDisplay
+{
+    private LocalDate rentStartDate;
+    private LocalDate rentEndDate;
+    private float rentInNoOfDays;
+    private double totalKilometerDrived;
+    private float totalBillToPay;
+
+    public VehicleRent(LocalDate rentStartDate, LocalDate rentEndDate, float rentInNoOfDays, double totalKilometerDrived, float totalBillToPay)
+    {
+        this.rentStartDate = rentStartDate;
+        this.rentEndDate = rentEndDate;
+        this.rentInNoOfDays = rentInNoOfDays;
+        this.totalKilometerDrived = totalKilometerDrived;
+        this.totalBillToPay = totalBillToPay;
+    }
+
+    public LocalDate getRentStartDate() {
+        return rentStartDate;
+    }
+
+    public void setRentStartDate(LocalDate rentStartDate) {
+        this.rentStartDate = rentStartDate;
+    }
+
+    public LocalDate getRentEndDate() {
+        return rentEndDate;
+    }
+
+    public void setRentEndDate(LocalDate rentEndDate) {
+        this.rentEndDate = rentEndDate;
+    }
+
+    public float getRentInNoOfDays() {
+        return rentInNoOfDays;
+    }
+
+    public void setRentInNoOfDays(float rentInNoOfDays) {
+        this.rentInNoOfDays = rentInNoOfDays;
+    }
+
+    public double getTotalKilometerDrived() {
+        return totalKilometerDrived;
+    }
+
+    public void setTotalKilometerDrived(double totalKilometerDrived) {
+        this.totalKilometerDrived = totalKilometerDrived;
+    }
+
+    public float getTotalBillToPay() {
+        return totalBillToPay;
+    }
+
+    public void setTotalBillToPay(float totalBillToPay) {
+        this.totalBillToPay = totalBillToPay;
+    }
+
+    @Override
+    public void print()
+    {
+
+    }
 }
