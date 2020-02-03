2ccc0b34835f951b7a88f7333c579e49917209d4
Philip <jhasondeleon@gmail.com>
2020-02-03 16:45:37 -0500

    [Philip] remove the error in constructor in Customer class

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -8,9 +8,8 @@ public class Customer extends Person
     private String city;
     private String vehicleRent;
 
-    public Customer(int id, String firstName, String lastName, Gender gender, LocalDate birthDate, int age, int mobileNumber, String emailId, String userName, String passWord, String address, String city, String vehicleRent)
-    {
-        super(id, firstName, lastName, gender, birthDate, age, mobileNumber, emailId, userName, passWord);
+    public Customer(int id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String passWord, String address, String city, String vehicleRent) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, passWord);
         this.address = address;
         this.city = city;
         this.vehicleRent = vehicleRent;
