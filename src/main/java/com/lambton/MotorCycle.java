959af9703f4eb6f5fdfb49549d56ec0f36143229
anmariya <anmariya1915@gmail.com>
2020-02-02 19:29:20 -0500

    [ANMARIYA]display function created on MotorCycle class

--- src/main/java/com/lambton/MotorCycle.java
+++ src/main/java/com/lambton/MotorCycle.java
@@ -2,11 +2,18 @@ package com.lambton;
 
 public class MotorCycle extends Vehicles
 {
-    private float MaximumSpeed;
+    private float maximumSpeed;
     private float milage;
     public MotorCycle(int vechicleId, String vehicleDetails, String manufacturerName, String insuranceProviderName, int noOfSeats, FuelTypes fuelTypes)
     {
         super(vechicleId, vehicleDetails, manufacturerName, insuranceProviderName, noOfSeats, fuelTypes);
+    }
+
+    public float getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    public void setMaximumSpeed(float maximumSpeed) { this.maximumSpeed = maximumSpeed;
     }
 
     public float getMilage() {
@@ -17,11 +24,17 @@ public class MotorCycle extends Vehicles
         this.milage = milage;
     }
 
-    public float getMaximumSpeed() {
-        return MaximumSpeed;
-    }
-
-    public void setMaximumSpeed(float maximumSpeed) {
-        MaximumSpeed = maximumSpeed;
+    public void display()
+    {
+        System.out.println("MotorCycle Identification Number : " + getVechicleId());
+        System.out.println("MotorCycle Details : "+getVehicleDetails());
+        System.out.println("MotorCycle Manufacturer's name : "+getManufacturerName());
+        System.out.println("Is MotorCycle Insured? : "+isVehicleIsInsured());
+        System.out.println("MotorCycle Insurance Provider's Name : "+getInsuranceProviderName());
+        System.out.println("MotorCycle Fuel Type : "+getFuelType());
+        System.out.println("MotorCycle Base Rate Per Day : "+getBaseRatePerDay());
+        System.out.println("MotorCycle Rate per Hour : "+getRatePerKilometer());
+        System.out.println("MotorCycle maximum speed : "+getMaximumSpeed());
+        System.out.println("MotorCycle milage: "+getMilage());
     }
 }
