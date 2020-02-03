2481c2d91db9cbe645a5e46b935f742d55ddb916
anmariya <anmariya1915@gmail.com>
2020-02-02 23:25:40 -0500

    [ANMARIYA]added Driver class  print function

--- src/main/java/com/lambton/Driver.java
+++ src/main/java/com/lambton/Driver.java
@@ -4,36 +4,32 @@ import java.time.LocalDate;
 
 public class Driver extends Person
 {
-    private String driverLicenceNo;
-    private boolean drivingHistoryCleared;
+    private String driverLicenceNumber;
+    private boolean isDrivingHistoryCleared;
     private float salary;
 
     public Driver(int id, String firstName, String lastName, Gender gender, LocalDate birthDate, int age, int mobileNumber, String emailId, String userName, String passWord, String driverLicenceNo, boolean drivingHistoryCleared, float salary)
     {
         super(id, firstName, lastName, gender, birthDate, age, mobileNumber, emailId, userName, passWord);
-        this.driverLicenceNo = driverLicenceNo;
-        this.drivingHistoryCleared = drivingHistoryCleared;
+        this.driverLicenceNumber = driverLicenceNo;
+        this.isDrivingHistoryCleared = isDrivingHistoryCleared;
         this.salary = salary;
     }
 
-    public String getDriverLicenceNo()
-    {
-        return driverLicenceNo;
+    public String getDriverLicenceNumber() {
+        return driverLicenceNumber;
     }
 
-    public void setDriverLicenceNo(String driverLicenceNo)
-    {
-        this.driverLicenceNo = driverLicenceNo;
+    public void setDriverLicenceNumber(String driverLicenceNumber) {
+        this.driverLicenceNumber = driverLicenceNumber;
     }
 
-    public boolean isDrivingHistoryCleared()
-    {
-        return drivingHistoryCleared;
+    public boolean isDrivingHistoryCleared() {
+        return isDrivingHistoryCleared;
     }
 
-    public void setDrivingHistoryCleared(boolean drivingHistoryCleared)
-    {
-        this.drivingHistoryCleared = drivingHistoryCleared;
+    public void setDrivingHistoryCleared(boolean drivingHistoryCleared) {
+        isDrivingHistoryCleared = drivingHistoryCleared;
     }
 
     public float getSalary()
@@ -44,5 +40,13 @@ public class Driver extends Person
     public void setSalary(float salary)
     {
         this.salary = salary;
+    }
+
+    @Override
+    public void print()
+    {
+            System.out.println("Driver Licence Number: " + getDriverLicenceNumber());
+            System.out.println("Salary: " + getSalary());
+            System.out.println("Is Driving History Cleared ?: " + isDrivingHistoryCleared());
     }
 }
